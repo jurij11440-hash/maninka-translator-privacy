4ad8c4d7846ead3e63a2be7d432bea09d7dd5653
jurij11440-hash <jurij11440@gmail.com>
2026-02-22 10:59:54 +0000

    Create src/main/java/com/example/maninka/MainActivity.java

---
+++ android/app/src/main/java/com/example/maninka/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.maninka;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
+    }
+}
